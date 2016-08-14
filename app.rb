d421dd4ab9b30317122e09c44dd9275ba5aedade
“reddrum” <anton.kranz@me.com>
2016-08-14 16:16:36 +0300

    Add rakefile, uodate gemfile

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "pizzashop.db"
+
+class Product < Activerecord::Base
+
+end
 
 get '/' do
 	erb :index
