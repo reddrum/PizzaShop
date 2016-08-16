db0f9cf10fcf7e879cd50b8ac7714f76758f2d4e
“reddrum” <anton.kranz@me.com>
2016-08-17 00:26:17 +0300

    Update

--- app.rb
+++ app.rb
@@ -19,9 +19,13 @@ get '/about' do
   erb :about
 end
 
+get '/cart' do
+  erb :cart
+end
+
 post '/cart' do
-  orders_input = params[:orders]
-  @items = parse_orders_input orders_input
+  @orders_input = params[:orders]
+  @items = parse_orders_input @orders_input
 
   @items.each do |item|
     # id, cnt
