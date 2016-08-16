1bdedcfe7964891ae2044af02cabdd5805b686f3
“reddrum” <anton.kranz@me.com>
2016-08-16 16:11:59 +0300

    Add cart_get_number_of_products function

--- public/app.js
+++ public/app.js
@@ -1,15 +1,15 @@
 function something()
 	{
-		var x = window.localStorage.getItem('bbb');
+		var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
 		x = x * 1 + 1;
 
-		window.localStorage.setItem('bbb', x);
+		window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
 		alert(x);
 
 	}
-	
+
 function add_to_cart(id)
 {
 	var key = "product_" + id;
@@ -18,3 +18,20 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 }	
+
+function cart_get_number_of_products()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // полуаем значение, аналог в ruby hh[key] = x 
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+	return cnt;
+}
